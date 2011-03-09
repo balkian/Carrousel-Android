699f7e153c7ce1a3bb772a8f377e24f06acc1e1f
Fernando Herrera <fherrera@onirica.com>
2011-03-09 22:55:28 +0100

    Iteracion 2: Servicio añadido

---
+++ src/com/onirica/carrousel/Results.java
@@ -0,0 +1,15 @@
+package com.onirica.carrousel;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Results extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
